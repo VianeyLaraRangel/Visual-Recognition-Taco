45cd26085a3a684bcad43e4af0527e860ea36fe4
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-08-01 20:40:05 -0500

    Añadiendo api y id del modelo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var fs = require('fs');
 //set IBM cloud credentials in this section
 var visualRecognition = new VisualRecognitionV3({
   version: '2019-05-29',
-  iam_apikey: 'Remplace aqui'
+  iam_apikey: 'l-K-egE7RoONxgRr-6qGMbKyGPa6RFzf5jFTcMi4eldE'
 });
 
 // Set The Storage Engine
@@ -72,7 +72,7 @@ app.post('/upload', (req, res) => {
         });
       } else {
         var images_file = fs.createReadStream(`./public/uploads/${req.file.filename}`);
-        var classifier_ids = ["Tiposdetaco_2107577272"];
+        var classifier_ids = ["TacosTacos_1721237988"];
         console.log("datos de la imagen -- " + images_file);
         //
           var params = {
